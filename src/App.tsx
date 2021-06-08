83e1d7f5f6a05e57c8c03eccae1faaa0dcb8cb8f
Alejandro Espinosa de los Monteros <alex.espinosadelosmonteros@gmail.com>
2021-06-07 22:34:16 -0500

    update readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Sidebar from './features/Sidebar';
 import Placeholder from './common/placeholder';
 import Contacts from './features/contacts';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
@@ -9,13 +9,14 @@ function App() {
       <Router>
         <Sidebar />
         <Switch>
-          <Route path='/pos' exact render={(rProps) => <Placeholder {...rProps} />} />
+          <Route path='/pos' render={(rProps) => <Placeholder {...rProps} />} />
           <Route path='/contacts' exact render={(rProps) => <Contacts {...rProps} />} />
           <Route path='/invoices' render={(rProps) => <Placeholder {...rProps} />} />
           <Route path='/inventory' render={(rProps) => <Placeholder {...rProps} />} />
           <Route path='/reports' render={(rProps) => <Placeholder {...rProps} />} />
           <Route path='/settings' render={(rProps) => <Placeholder {...rProps} />} />
           <Route path='/imring' render={(rProps) => <Placeholder {...rProps} />} />
+          <Redirect from='/' to='pos' />
         </Switch>
       </Router>
     </>
